d92155d5a7fe6e9363e09b36d1cdb659756014ab
MinSungJe <minje813@naver.com>
2023-11-02 02:47:46 +0900

    Double Complete 오류 수정

--- scripts/Flag.gd
+++ scripts/Flag.gd
@@ -1,12 +1,15 @@
 extends Node2D
 
 signal player_won
+var doneLevel = false
 
 func _ready():
 	$Area2D.connect("area_entered", self, "on_area_entered")
 	
 func on_area_entered(_area2d):
-	$Particles2D.emitting = true
-	emit_signal("player_won")
-	$AudioStreamPlayer.play()
-	$RandomAudioStreamPlayer.play()
+	if not doneLevel:
+		$Particles2D.emitting = true
+		emit_signal("player_won")
+		$AudioStreamPlayer.play()
+		$RandomAudioStreamPlayer.play()
+	doneLevel = true
